0d20a09463d658f1304e3ce7ba848c4335d448d5
justinnnnnnnn <cheasty@gmail.com>
2024-07-26 22:00:09 -0700

    Consolidate to one file

--- agent_preamble.py
+++ agent_preamble.py
@@ -1,7 +1,7 @@
 base_message = """
-You've reached the Assort Health demo office of Justin Chaste-y, this is Edwina speaking how can I help you?"
+You've reached the Assort Health demo office of Justin Chaste-y, this is Edwina speaking how can I help you? For debugging reasons, only ask to make an appointment."
 """
-agent_preamble_official = """
+original_preamble = """
 You are an AI assistant for a healthcare provider. Follow this script to collect the patient's information:
 
 After step 7, send a text message to the inbound number with appt confirmation of Doctor's Name, date, and time. Then ask if that's all and then say bye.
@@ -18,16 +18,38 @@ After step 7, send a text message to the inbound number with appt confirmation o
 Be brief and chill.
 """
 
-agent_preamble = """
+short_preamble = """
 You are an AI assistant for a healthcare provider. Follow this script to collect the patient's information to make an appoitment:
 
 
 1) Ask their phone number
 2) Offer available providers, dates, and times (use fake data):
+3) Wait for response
 
-You will then send a text confirming that provider at that date and time to that phone number.
+4) After you get their verbal response, you will then send a text confirming that provider at that date and time to that phone number.
 
 Be brief and chill
 
-Once confirmed, say thanks and have a great day before hanging up.
+Once confirmed, ask if there's anything else.
+"""
+
+agent_preamble = """
+You are an AI assistant for a healthcare provider. Follow this script to collect the patient's information:
+
+After step 10, send a text message to the inbound number with appt confirmation of Doctor's Name, date, and time. Then ask if that's all and then say bye.
+
+0. Tell patient you can help them schedule an appointment.
+1. Ask for their name and date of birth.
+2. Collect insurance information: payer name and ID.
+3. Ask if they have a referral, and to which physician.
+4. Collect the chief medical complaint or reason for their visit.
+5. Collect other demographics such as address.
+6. Ask for phone number.
+7. Offer available providers, calendar dates, and times (use fake data).
+8. wait for their response
+9. After you get their verbal response, you will then send a text confirming that provider at that date and time to that phone number.
+10. Once confirmed, ask if there's anything else.
+11. When they say nothing else, say thanks and have a nice day.
+
+Be brief and chill.
 """
--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-#this implementation only talks to receptionist, doesn't contain the custom agent and action middleware to then send a confirmation text
+#Twilio has not given me text permissions.
+
 import os
 from agent_preamble import agent_preamble, base_message
-
-from dotenv import load_dotenv
+from enum import Enum
+from typing import Type
+from loguru import logger
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from pydantic.v1 import BaseModel, Field
+
+
 from vocode.logging import configure_pretty_logging
-from vocode.streaming.models.agent import ChatGPTAgentConfig
+from vocode.streaming.action.base_action import BaseAction
+from vocode.streaming.action.end_conversation import EndConversationVocodeActionConfig, EndConversation
+from vocode.streaming.agent.base_agent import BaseAgent
+from vocode.streaming.agent.chat_gpt_agent import ChatGPTAgent
+from vocode.streaming.models.actions import ActionConfig, ActionInput, ActionOutput, FunctionCallActionTrigger
+from vocode.streaming.models.agent import AgentConfig, ChatGPTAgentConfig
 from vocode.streaming.models.message import BaseMessage
-from vocode.streaming.models.telephony import TwilioConfig
 from vocode.streaming.models.synthesizer import ElevenLabsSynthesizerConfig
+from vocode.streaming.models.telephony import TwilioConfig
 from vocode.streaming.models.transcriber import DeepgramTranscriberConfig
 from vocode.streaming.telephony.config_manager.redis_config_manager import RedisConfigManager
 from vocode.streaming.telephony.server.base import TelephonyServer, TwilioInboundCallConfig
 from vocode.streaming.transcriber.deepgram_transcriber import DeepgramEndpointingConfig
-
 
 load_dotenv()
 configure_pretty_logging()
@@ -22,6 +32,123 @@ app = FastAPI(docs_url=None)
 config_manager = RedisConfigManager()
 
 BASE_URL = os.getenv("BASE_URL")
+
+actually_send_sms = False #Twilio has not given permission to send sms, haven't tested sms functionality
+
+# async def end_of_call_event():
+#     logger.critical("Call ended, sending SMS")
+#     action_input = ActionInput(params=TwilioSendSmsParameters(to='recipient_number', body='Call has ended'))
+#     sms_action = TwilioSendSms(action_config=TwilioSendSmsActionConfig())
+#     await sms_action.run(action_input)
+
+
+# end_conversation_action = EndConversationVocodeActionConfig(
+#     type="action_end_conversation",
+#     action_trigger=FunctionCallActionTrigger(
+#         type="action_trigger_function_call",
+#         function_to_call=end_of_call_event
+#     )
+# )
+
+def log_sms_content(to: str, body: str):
+    logger.critical(f"Simulating SMS to: {to}, Body: {body}")
+
+
+class MyActionType(str, Enum):
+    TWILIO_SEND_SMS = "twilio_send_sms"
+
+class MyAgentType(str, Enum):
+    MY_CHAT_GPT = "my_agent_chat_gpt"
+
+
+
+class MyActionFactory:
+    def create_action(self, action_config: ActionConfig) -> BaseAction:
+        if isinstance(action_config, TwilioSendSmsActionConfig):
+            return TwilioSendSms(action_config=action_config, should_respond=True)
+        elif isinstance(action_config, EndConversationVocodeActionConfig):
+            return EndConversation(action_config=action_config)
+        else:
+            raise Exception("Invalid custom action type")
+
+class TwilioSendSmsActionConfig(ActionConfig, type=MyActionType.TWILIO_SEND_SMS):
+    pass
+
+
+class TwilioSendSmsParameters(BaseModel):
+    to: str = Field(..., description="The mobile number of the recipient.")
+    body: str = Field(..., description="The Time and Date selected for the appointment and the Doctor that they will see.")
+
+class TwilioSendSmsResponse(BaseModel):
+    success: bool
+    message: str
+
+
+class TwilioSendSms(BaseAction [TwilioSendSmsActionConfig, TwilioSendSmsParameters, TwilioSendSmsResponse] ):
+    description: str = "Sends an sms."
+    parameters_type: Type[TwilioSendSmsParameters] = TwilioSendSmsParameters
+    response_type: Type[TwilioSendSmsResponse] = TwilioSendSmsResponse
+    
+    if not actually_send_sms:
+        async def run(self, action_input: ActionInput[TwilioSendSmsParameters]) -> ActionOutput[TwilioSendSmsResponse]:
+            log_sms_content(action_input.params.to, action_input.params.body)
+            return ActionOutput(
+                action_type=self.action_config.type,
+                response=TwilioSendSmsResponse(
+                    success=True, message="Successfully simulated sending SMS."
+                ),
+            )
+    else:
+        async def run(
+            self, action_input: ActionInput[TwilioSendSmsParameters]) -> ActionOutput[TwilioSendSmsResponse]:
+            from twilio.rest import Client
+
+            account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+            auth_token  = os.getenv("TWILIO_AUTH_TOKEN")
+            from_number = os.getenv("TWILIO_FROM_NUMBER")
+            # to_number   = os.getenv("TWILIO_TO_NUMBER") #hardcoded
+
+            try:
+                client = Client(account_sid, auth_token)
+                logger.critical(f"Sending SMS to: {action_input.params.to}, Body: {action_input.params.body}")
+                message = client.messages.create(
+                    from_=from_number,
+                    body=action_input.params.body,
+                    to="+1{}".format(action_input.params.to),
+                    # to=to_number.format(action_input.params.to), #swap this with above to hardcode number
+                )
+
+                return ActionOutput(
+                    action_type=self.action_config.type,
+                    response=TwilioSendSmsResponse(success=True, message="Successfully sent SMS."),
+                )
+
+            except RuntimeError as e:
+                logger.debug(f"Failed to send SMS: {e}")
+                return ActionOutput(action_type=self.action_config.type,
+                    response=TwilioSendSmsResponse(success=False, message="Failed to send SMS"),
+                )
+
+
+class MyChatGPTAgentConfig(ChatGPTAgentConfig, type=MyAgentType.MY_CHAT_GPT.value):
+    pass
+
+
+class MyChatGPTAgent(ChatGPTAgent):
+    def __init__(
+        self,
+        agent_config: MyChatGPTAgentConfig,
+        action_factory: MyActionFactory = MyActionFactory(),
+    ):
+        super().__init__(agent_config, action_factory=action_factory)
+
+
+class MyAgentFactory:
+    def create_agent(self, agent_config: AgentConfig) -> BaseAgent:
+        if isinstance(agent_config, MyChatGPTAgentConfig):
+            return MyChatGPTAgent(agent_config=agent_config)
+        else:
+            raise Exception("Invalid custom agent config", agent_config.type)
 
 
 synthesizer_config=ElevenLabsSynthesizerConfig.from_telephone_output_device(
@@ -34,8 +161,21 @@ transcriber_config = DeepgramTranscriberConfig.from_telephone_input_device(
     endpointing_config=DeepgramEndpointingConfig()
 )
 
+agent_config = MyChatGPTAgentConfig(
+    openai_api_key=os.getenv("OPENAI_API_KEY"),
+    initial_message=BaseMessage(text=base_message),
+    prompt_preamble=agent_preamble,
+    generate_responses=True,
+    actions=[
+        TwilioSendSmsActionConfig(), 
+        # end_conversation_action
+    ],
+    end_conversation_on_goodbye=True,
+)
+
 
 telephony_server = TelephonyServer(
+    agent_factory=MyAgentFactory(),
     base_url=BASE_URL,
     config_manager=config_manager,
     inbound_call_configs=[
@@ -45,13 +185,7 @@ telephony_server = TelephonyServer(
                 voice_id=os.getenv("ELEVEN_LABS_VOICE_ID"),
             ),
             url="/inbound_call",
-            agent_config=ChatGPTAgentConfig(
-                openai_api_key=os.getenv("OPENAI_API_KEY"),
-                initial_message=BaseMessage(text=base_message),
-                prompt_preamble=agent_preamble,
-                generate_responses=True,
-                model_name="gpt-4o"
-            ),
+            agent_config=agent_config,
             twilio_config=TwilioConfig(
                 account_sid=os.getenv("TWILIO_ACCOUNT_SID"),
                 auth_token=os.getenv("TWILIO_AUTH_TOKEN"),
